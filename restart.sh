2f3b38014fdc5752f91c219d588b619a421dd974
Lars Repenning <lars.repenning@gmx.de>
2024-05-27 21:56:45 +0200

    fix for un-/install/restart scripts

--- restart.sh
+++ restart.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-kill $(pgrep -f "python $SCRIPT_DIR/dbus-shelly-1pm-pvinverter.py")
+echo $SCRIPT_DIR
+
+PID=$(pgrep -f $SCRIPT_DIR/vestelEvc04Service.py)
+echo $PID
+kill $PID
--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,5 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVICE_NAME=$(basename $SCRIPT_DIR)
 
-rm /service/$SERVICE_NAME
-kill $(pgrep -f 'supervise dbus-shelly-1pm-pvinverter')
 chmod a-x $SCRIPT_DIR/service/run
 ./restart.sh
