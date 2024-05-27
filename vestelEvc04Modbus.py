b6d71f6b0e442a1c8687f75eca7292df47dd2858
Lars Repenning <lars.repenning@gmx.de>
2024-05-27 19:48:49 +0200

    inital commit

---
+++ vestelEvc04Modbus.py
@@ -0,0 +1,192 @@
+from pymodbus.client import ModbusTcpClient
+
+import logging
+import sys
+import time
+
+def intToTime(time:int):
+    hour = int(time / 10000)
+    minute = int(time/100%60)
+    second = time%60
+    return  "%02d:%02d:%02d" % (hour, minute, second)
+
+def getU32(bytes, highByte:int):
+    return (bytes[highByte] << 16) + bytes[highByte+1]
+
+def convertToString(bytes):
+    return ''.join(chr(byte) for byte in bytes if byte != 0)
+
+class Evc04Charger():
+    def __init__(self, host:str):
+        self.host = host
+        self.client =None
+        self.newMaxCurrent = None
+        pass
+
+    def connect(self):
+        if(self.client == None):
+            self.client = ModbusTcpClient(host=self.host, port=502, unit_id=255)
+            
+        time.sleep(0.01)
+        # if not self.client.connected():
+        self.client.connect()
+        return 
+
+    def close(self):
+        self.client.close()
+
+    def getSystemInfo(self):
+        # self.connect()
+        self.systemInfo = {}
+        self.systemInfo["serial"] = convertToString(self.client.read_input_registers(100, 25).registers)  
+        self.systemInfo["chargepointId"] = convertToString(self.client.read_input_registers(130, 50).registers)
+        self.systemInfo["brand"] = convertToString(self.client.read_input_registers(199, 10).registers)
+        
+        self.model = convertToString(self.client.read_input_registers(210, 5).registers)
+        self.systemInfo["model"] = self.model
+
+        self.firmware = convertToString(self.client.read_input_registers(230, 50).registers)
+        self.systemInfo["firmware"] = self.firmware
+
+        logging.info("Connected to device %s" % self.systemInfo)
+        # self.close()
+
+    def readSystemState(self):
+        systemState = {}
+        systemStateResisters = self.client.read_input_registers(1000, 7)
+        self.cpState = systemStateResisters.registers[0]
+        systemState["state"] = self.cpState
+        # 0: "Available",
+        # 1: "Preparing",
+        # 2: "Charging",
+        # 3: "SuspendedEVSE",
+        # 4: "SuspendedEV",
+        # 5: "Finishing",
+        # 6: "Reserved",
+        # 7: "Unavailable",
+        # 8: "Faulted"
+
+        systemState["chargingState"] = systemStateResisters.registers[1]
+        # 0: Not Charging, State Ax, Bx, Dx or C1
+        # 1: Charging, state C2
+
+        systemState["equipmentState"] = systemStateResisters.registers[2]
+        # 0: Initializing
+        # 1: Running
+        # 2: Fault
+        # 3: Disabled
+        # 4: Updating
+        self.cableState = systemStateResisters.registers[4]
+        systemState["cableState"] = self.cableState
+        # 0: Cable not connected
+        # 1: Cable connected, vehicle not connected
+        # 2: Cable connected, vehicle connected
+        # 3: Cable connected, vehicle connected, cable locked
+
+        systemState["evseFaultCode"] = systemStateResisters.registers[6]
+        # 0: No fault
+        # Other: Fault code
+        return systemState
+
+    def readPowerData(self):
+        powerData = {}
+        powerDataRegisters = self.client.read_input_registers(1008, 11)
+        powerData["currentL1"] = powerDataRegisters.registers[0]
+        powerData["currentL2"] = powerDataRegisters.registers[2]
+        powerData["currentL3"] = powerDataRegisters.registers[4]
+
+        powerData["voltageL1"] = powerDataRegisters.registers[6]
+        powerData["voltageL2"] = powerDataRegisters.registers[8]
+        powerData["voltageL3"] = powerDataRegisters.registers[10]
+
+        powerDataRegisters = self.client.read_input_registers(1020, 14)
+        powerData["powerTotal"] = getU32(powerDataRegisters.registers, 0)
+        powerData["powerL1"]  = getU32(powerDataRegisters.registers, 4)
+        powerData["powerL2"]  = getU32(powerDataRegisters.registers, 8)
+        powerData["powerL3"]  = getU32(powerDataRegisters.registers, 12)
+        powerData["powerL3"]  = getU32(powerDataRegisters.registers, 12)
+        return powerData
+
+    def readSessionData(self):
+        sessionData = {}
+        sessionDataRegisters = self.client.read_input_registers(1502, 12)
+        sessionData["sessionEnergy"] = getU32(sessionDataRegisters.registers, 0)
+        sessionData["sessionStart"] = intToTime(getU32(sessionDataRegisters.registers, 2))
+        sessionData["sessionDuration"] = getU32(sessionDataRegisters.registers, 6)
+        sessionData["sessionEnd"] = intToTime(getU32(sessionDataRegisters.registers, 10))
+        return sessionData
+    
+    def readMaxCurrent(self):
+        result = {}
+        registers = self.client.read_holding_registers(5004, 1).registers
+        logging.debug("Max Current %s" % registers)
+        self.maxCurrent = registers[0]
+        result['maxCurrent'] = self.maxCurrent
+        return result
+    
+    def updateValues(self):
+        # set bit to mark master as alive
+        self.client.write_register(6000, 1)
+
+        if self.newMaxCurrent != None:
+            logging.info("New max current will be set to: %s " % self.newMaxCurrent)
+            self.client.write_register(5004,self.newMaxCurrent)
+            self.newMaxCurrent = None
+
+    def setMaxCurrent(self, value):
+        self.newMaxCurrent = value
+
+    def readRelevantData(self):
+        self.connect()
+        try:
+            result = {}
+            result.update(self.readSystemState())
+            result.update(self.readPowerData())
+            result.update(self.readSessionData())
+            result.update(self.readMaxCurrent())
+            logging.info(result)  # Print the value read   
+        except AttributeError as msg:
+            logging.error(msg)
+            # self.close()
+            return None
+        # self.close()
+        return result
+    
+
+    def getFirmwareversion(self):
+        return self.firmware
+    
+    def getModel(self):
+        return self.model
+
+    def getVrmStatus(self):
+        cableState = self.cableState
+        if cableState == 0:
+                return 0
+        if cableState == 1:
+                return 0
+        if cableState == 2 or 3:
+            logging.debug("Cable state: %s" % cableState)
+            cpState = self.cpState
+            if cpState == 0:
+                return 1
+            if cpState == 1:
+                return 6
+            if cpState == 2:
+                return 2
+            if cpState == 3:
+                return 6
+            if cpState == 4:
+                return 20
+            if cpState == 5:
+                return 3
+            if cpState == 6 or 7 or 8:
+                return 10
+                
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    charger = Evc04Charger("192.168.178.96")
+    logging.info(charger.getSystemInfo())
+    charger.readRelevantData()
---
+++ vestelEvc04Service.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+
+"""
+A class to put a simple service on the dbus, according to victron standards, with constantly updating
+paths. See example usage below. It is used to generate dummy data for other processes that rely on the
+dbus. See files in dbus_vebus_to_pvinverter/test and dbus_vrm/test for other usage examples.
+
+To change a value while testing, without stopping your dummy script and changing its initial value, write
+to the dummy data via the dbus. See example.
+
+https://github.com/victronenergy/dbus_vebus_to_pvinverter/tree/master/test
+"""
+from gi.repository import GLib
+import platform
+import argparse
+import logging
+import sys
+import os
+import pymodbus
+import configparser # for config/ini file
+from  vestelEvcModbus import Evc04Charger
+
+# our own packages from victron
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), '/opt/victronenergy/dbus-systemcalc-py/ext/velib_python'))
+from vedbus import VeDbusService
+
+class VestelEvc04Service(object):
+    def __init__(self, servicename, deviceinstance, paths, productname='Vestel EVC04', connection='Vestel EVC04 Service'):
+        config = self._getConfig()
+
+        deviceinstance = int(config['DEFAULT']['Deviceinstance'])
+        host = config['ONPREMISE']['Host']
+        
+        self._dbusservice = VeDbusService("{}.evc04_{:02d}".format(servicename, deviceinstance))
+        self._paths = paths
+
+        self.evc04Charger = Evc04Charger(host)
+        self.evc04Charger.connect()
+        
+        self.evc04Charger.getSystemInfo()
+
+        logging.debug("%s /DeviceInstance = %d" % (servicename, deviceinstance))
+
+        # Create the management objects, as specified in the ccgx dbus-api document
+        self._dbusservice.add_path('/Mgmt/ProcessName', __file__)
+        self._dbusservice.add_path('/Mgmt/ProcessVersion', 'Unkown version, and running on Python ' + platform.python_version())
+        self._dbusservice.add_path('/Mgmt/Connection', connection)
+
+        # Create the mandatory objects
+        self._dbusservice.add_path('/DeviceInstance', deviceinstance)
+        self._dbusservice.add_path('/ProductId', 0)
+        self._dbusservice.add_path('/ProductName', productname)
+        self._dbusservice.add_path('/FirmwareVersion', self.evc04Charger.getFirmwareversion())
+        self._dbusservice.add_path('/Model', self.evc04Charger.getModel())
+
+        for path, settings in self._paths.items():
+            self._dbusservice.add_path(path, settings['initial'], writeable=False, onchangecallback=self._handlechangedvalue)
+
+        self._dbusservice.add_path('/SetCurrent', None, writeable=True, onchangecallback=self._handleCurrentChanged)
+
+        refreshRate = int(config['DEFAULT']['RefreshRate'])
+        MIN_REFRESH_RATE = 250
+        if not refreshRate or refreshRate < MIN_REFRESH_RATE:
+            refreshRate = MIN_REFRESH_RATE
+        GLib.timeout_add(refreshRate, self._update) # pause at least 250ms before the next request
+
+    def update(self, path, value):
+        self._dbusservice[path] = value
+        logging.debug("%s: %s" % (path, value))
+
+    def _update(self):
+        try:
+            # self.evc04Charger.connect()
+            evc04Data = self.evc04Charger.readRelevantData()
+            if evc04Data == None:  
+                logging.debug("Will skip an update")
+                return True
+            
+            self.update('/Ac/Energy/Forward', evc04Data['sessionEnergy']/1000)
+            self.update('/Ac/Power', evc04Data['powerTotal'])
+            self.update('/Ac/L1/Power', evc04Data['powerL1'])
+            self.update('/Ac/L2/Power', evc04Data['powerL2'])
+            self.update('/Ac/L3/Power', evc04Data['powerL3'])
+            self.update('/Status', self.evc04Charger.getVrmStatus())
+            self.update('/MaxCurrent', self.evc04Charger.maxCurrent)
+            self.update('/ChargingTime', evc04Data['sessionDuration'])
+
+            self.evc04Charger.updateValues()
+        except pymodbus.exceptions.ConnectionException as msg:
+            logging.warn("Cannot connect to evc04 %s" % self.evc04Charger.host)
+        # self.evc04Charger.close()
+        return True
+
+    def _handlechangedvalue(self, path, value):
+        logging.warn("someone else updated %s to %s" % (path, value))
+        return True # accept the change
+    
+    def _handleCurrentChanged(self, path, value):
+        logging.info("Max current will be changed to %s" % value)
+        self.evc04Charger.setMaxCurrent(value)
+        return True
+
+    def _getConfig(self):
+        config = configparser.ConfigParser()
+        config.read("%s/config.ini" % (os.path.dirname(os.path.realpath(__file__))))
+        return config;
+
+# === All code below is to simply run it from the commandline for debugging purposes ===
+
+# It will created a dbus service called com.victronenergy.pvinverter.output.
+# To try this on commandline, start this program in one terminal, and try these commands
+# from another terminal:
+# dbus com.victronenergy.pvinverter.output
+# dbus com.victronenergy.pvinverter.output /Ac/Energy/Forward GetValue
+# dbus com.victronenergy.pvinverter.output /Ac/Energy/Forward SetValue %20
+#
+# Above examples use this dbus client: http://code.google.com/p/dbus-tools/wiki/DBusCli
+# See their manual to explain the % in %20
+
+def main():
+      #configure logging
+    logging.basicConfig(      format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                                datefmt='%Y-%m-%d %H:%M:%S',
+                                level=logging.INFO,
+                                handlers=[
+                                    logging.FileHandler("%s/current.log" % (os.path.dirname(os.path.realpath(__file__)))),
+                                    logging.StreamHandler()
+                                ])
+
+
+    from dbus.mainloop.glib import DBusGMainLoop
+    # Have a mainloop, so we can send/receive asynchronous calls to and from dbus
+    DBusGMainLoop(set_as_default=True)
+
+    pvac_output = VestelEvc04Service(
+        servicename='com.victronenergy.evcharger',
+        deviceinstance=0,
+        paths={
+    '/Ac/Power':                            {'initial': 0},
+    '/Ac/L1/Power':                         {'initial': 0},
+    '/Ac/L2/Power':                         {'initial': 0},
+    '/Ac/L3/Power':                         {'initial': 0},
+    '/Ac/Energy/Forward':                   {'initial': 1},
+    '/Current':                             {'initial': 6},
+    '/MaxCurrent':                          {'initial': 16},
+
+    '/AutoStart':                           {'initial': 1},
+    '/ChargingTime':                        {'initial': 0},
+    '/EnableDisplay':                       {'initial': 1},
+    '/Mode':                                {'initial': 0},
+    '/Role':                                {'initial': 0},
+    '/StartStop':                           {'initial': 1},
+    '/Position':                            {'initial': 1},
+    
+    '/Status':                              {'initial': 1}
+        })
+
+    logging.info('Connected to dbus, and switching over to GLib.MainLoop() (= event based)')
+    mainloop = GLib.MainLoop()
+    mainloop.run()
+
+
+if __name__ == "__main__":
+    main()
